e124f2c8858bcc750b2f95737a64ee548a6c6387
Georgiy <g.manolov@web.de>
2024-04-06 17:27:14 +0200

    Registration,Login,Post,Contact Us

--- src/test/java/e2e/tests/Georgiy/EditUserDataProfileTest.java
+++ src/test/java/e2e/tests/Georgiy/EditUserDataProfileTest.java
@@ -81,7 +81,7 @@ public class EditUserDataProfileTest extends TestBase {
         editUserForm.waitForLoading();
         editUserForm.saveButtonClick();
         editUserForm.waitForLoading();
-        //checkEditUserData(editUserForm, editName, editSurname, editFormattedDate, editPhone);
+        checkEditUserData(editUserForm, editName, editSurname, editFormattedDate, editPhone);
 
         editPasswordForm = new EditPasswordForm(app.driver);
         editPasswordForm.changePassword(oldPassword, newPassword, confirmNewPassword);
