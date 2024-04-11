a2524ac933e71dd756b6fba2c5134f4e6c9e0ed2
Georgiy <g.manolov@web.de>
2024-04-11 17:12:43 +0200

    Registration,Login,Post,Contact Us

--- src/test/java/e2e/tests/post/UserCanCreateANewPost.java
+++ src/test/java/e2e/tests/post/UserCanCreateANewPost.java
@@ -65,7 +65,7 @@ public class UserCanCreateANewPost extends TestBase {
         String title = "My first post";
         String description = "Pice";
         String content = faker.lorem().sentence(20);
-        String folderPath = "src/test/java/resources";
+        String folderPath = "src/test/java/resources/5204092180870848359_121.jpg";
 
         loginPage = new LoginPage(app.driver);
         loginPage.waitForLoading();
--- src/test/java/integration/tests/login/LoginApiTest.java
+++ src/test/java/integration/tests/login/LoginApiTest.java
@@ -2,12 +2,15 @@ package integration.tests.login;
 
 
 import integration.pages.user.UserApi;
+import integration.pages.user.UserInfo;
+import io.qameta.allure.*;
+import io.restassured.path.json.JsonPath;
 import org.testng.annotations.Test;
 
 public class LoginApiTest {
 
     UserApi userApi;
-
+    UserInfo userInfo;
 
     @Test
     public void userCanLogin() {
@@ -16,11 +19,21 @@ public class LoginApiTest {
 
 
         userApi = new UserApi();
-        userApi.login(email, password, 200);
+        String token = userApi.login(email, password, 200);
+        userInfo = new UserInfo(token);
+        String userJson = userInfo.getUser();
+        JsonPath object = new JsonPath(userJson);
+        String userId = object.getString("id");
+        System.out.println(userId);
+
 
     }
 
-    @Test
+    @Feature(value= "User login")
+    @Story(value = "User can login with role user")
+    @Description(value = "User can not login")
+    @Severity(SeverityLevel.BLOCKER)
+    @Test(description = "User can not login wit invalid email")
     public void userCanNotLoginWitInvalidEmail() {
         String email = "gpower@gmail.com";
         String password = "GPower3333";
