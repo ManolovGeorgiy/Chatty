35ec8fa5f88a94190e0abbf9f766a53dfa7f959c
OksanaPovkh <133179018+OksanaPovkh@users.noreply.github.com>
2024-04-14 13:53:45 -0400

    edited e2e and api

--- src/test/java/integration/pages/profile/ProfileApi.java
+++ src/test/java/integration/pages/profile/ProfileApi.java
@@ -1,34 +1,69 @@
 package integration.pages.profile;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.javafaker.Faker;
 import integration.ApiBase;
+import integration.schemas.UserUpdateReq;
+import io.qameta.allure.Step;
+import io.restassured.response.Response;
 
 public class ProfileApi extends ApiBase {
 
-    public class Profile {
+    //private String token;
 
+    public void CreateProfile(String token) {
+        super(token);
 
-        public String avatarUrl;
-        public String name;
-        public String surname;
-        public String birthDate;
-        public String phone;
-        public String gender;
-        public String backgroundUrl;
-        public boolean blocked;
+    }
 
-        public Profile createProfile() {
-            Profile profile = new Profile();
-            profile.avatarUrl = "https://example.com/avatar.jpg";
-            profile.name = "John";
-            profile.surname = "Doe";
-            profile.birthDate = "1990-01-01";
-            profile.phone = "+1234567890";
-            profile.gender = "Male";
-            //profile.backgroundUrl = "https://example.com/background.jpg";
-            profile.blocked = false;
+    Response response;
+    Faker faker = new Faker();
+    UserUpdateReq userUpdateReq;
+    String name = "er";
+    String surname = "er";
+    String birthDate = "" ;
+    String phone = "";
+    String gender = "";
+    String backgroundUrl = "";
+    String blocked = "";
 
-            return profile;
+    //String imageUrl = "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg";
+
+    public UserUpdateReq createUserProfileForm (String userUpdateReq){
+        this.userUpdateReq = new UserUpdateReq();
+        this.userUpdateReq.setName(name);
+        this.userUpdateReq.setSurname(surname);
+        this.userUpdateReq.setBirthDate(birthDate);
+        this.userUpdateReq.setPhone(phone);
+        this.userUpdateReq.setGender(gender);
+        this.userUpdateReq.setBackgroundUrl(backgroundUrl);
+        this.userUpdateReq.setBlocked(Boolean.parseBoolean(blocked));
+
+        return this.userUpdateReq;
+    }
+    public Response createAUserUpdateReq(int code,String postCreateReq) {
+        String endpoint = "/api/users/{id}";
+        Object body = createUserProfileForm(String.valueOf(userUpdateReq));
+        response = postRequest(endpoint, code, body);
+        return response;
+    }
+    @Step("Create profile")
+    public String createProfile(int code,UserUpdateReq userUpdateReq) throws JsonProcessingException {
+        String endpoint = "/api/users/{id}";
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonRequestBody = objectMapper.writeValueAsString(userUpdateReq);
+        response = postRequest(endpoint,code,jsonRequestBody);
+        switch (response.getStatusCode()) {
+            case 200:
+                return  response.asString();
+            case 400:
+                return "Bad Request: " + response.jsonPath().getString("message");
+            case 401:
+                return "Unauthorized: " + response.jsonPath().getString("message");
+
+            default:
+                return "Unexpected status code: " + response.getStatusCode() + ". Response: " + response.asString();
         }
-
     }
 }
