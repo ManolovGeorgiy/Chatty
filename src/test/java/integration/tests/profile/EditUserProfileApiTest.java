8c21364b090fdd4639b4bef2128262d2aa84bc8f
OksanaPovkh <133179018+OksanaPovkh@users.noreply.github.com>
2024-04-15 21:29:03 -0400

    AdminPanel test

--- src/test/java/integration/tests/profile/EditUserProfileApiTest.java
+++ src/test/java/integration/tests/profile/EditUserProfileApiTest.java
@@ -5,5 +5,5 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class EditUserProfileApiTest {
+public class EditUserProfileApiTest{
 }
