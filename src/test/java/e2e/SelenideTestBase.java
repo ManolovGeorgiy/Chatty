41b00133b37d76da66a73d925f0ba24d7cdb0ee9
Georgiy <g.manolov@web.de>
2024-04-18 19:25:46 +0200

    Registration,Login,Post,Contact Us

---
+++ src/test/java/e2e/SelenideTestBase.java
@@ -0,0 +1,28 @@
+package e2e;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import config.Config;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideTestBase {
+    private final String URL = new Config().getProjectUrl();
+    private final String BROWSER = new Config().getBrowser();
+
+    @BeforeMethod
+    public void setUp() {
+        Configuration.browser = BROWSER;
+        Configuration.browserSize = "1920x1080";
+        open(URL);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        clearBrowserCookies();
+        clearBrowserLocalStorage();
+        closeWebDriver();
+    }
+}
---
+++ src/test/java/e2e/pages/adminPanel/AdminPanelPageNatalie.java
@@ -0,0 +1,10 @@
+package e2e.pages.adminPanel;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$x;
+
+
+public class AdminPanelPageNatalie {
+    public SelenideElement adminPanelNatalie = $x("//div[@class='admin-panel__users-section']");
+}
