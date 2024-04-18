db0ca73ae733d1522768570e0dddf027aa77ca0c
Georgiy <g.manolov@web.de>
2024-04-18 21:52:01 +0200

    Api test

--- src/test/java/integration/tests/adminApi/AdminEditDeleteUser.java
+++ src/test/java/integration/tests/adminApi/AdminEditDeleteUser.java
@@ -36,11 +36,11 @@ public class AdminEditDeleteUser  {
     UserUpdateReq userUpdateReq;
     DeleteUserApi deleteUserApi;
 
-    @Feature(value = "Edit User")
-    @Story(value = "Admin can edit User")
-    @Description(value = "Admin can edit User")
+    @Feature(value = "Deleted User")
+    @Story(value = "Admin can delete User")
+    @Description(value = "Admin can delete User")
     @Severity(SeverityLevel.BLOCKER)
-    @Test(description = "Admin can Edit new User")
+    @Test(description = "Admin can delete new User")
     public void adminCanDeleteUser() throws JsonProcessingException {
 
         String email = "bobo@gmail.com";
@@ -103,46 +103,6 @@ public class AdminEditDeleteUser  {
         deleteUserApi.deleteUser(204,userId);
 
 
-
-
-//    }
-//    @Feature(value = "Delete User")
-//    @Story(value = "Admin can Delete User")
-//    @Description(value = "Admin can Delete User")
-//    @Severity(SeverityLevel.BLOCKER)
-//    @Test(description = "Admin can Delete new User")
-//    public void adminCanLoginAndDeleteUser() {
-//        String email = generateRandomEmail();
-//        String password = "Boba9876";
-//        String confirmPassword = "Boba9876";
-//        String role = "user";
-//
-//        userApi = new UserApi();
-//        userApi.registration(email, password, confirmPassword, role, 201);
-//
-//        String emailLogin = "Boba1234@mail.ru";
-//        String passwordLogin = "Boba1234";
-//        String deleteUserEndpoint = "/api/users";
-//
-//        userApi = new UserApi();
-//        String token = userApi.login(emailLogin, passwordLogin, 200);
-//
-//        UserInfoDto userInfo = getUserByEmail(token, email, 200).get(0);
-//        assertNotNull(userInfo);
-//
-//        RestAssured.given()
-//                .spec(spec)
-//                .header("Authorization", "Bearer " + token)
-//                .when()
-//                .body(userInfo)
-//                .log().all()
-//                .delete(deleteUserEndpoint + "/" + userInfo.getId())
-//                .then().log().all()
-//                .extract().response();
-//
-//        checkUserDelete(token, email, 404);
-//
-//    }
     }
 
 }
